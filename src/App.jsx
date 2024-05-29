25aa2a558097ea54fa8d051642006b0a38ca01bf
Caramel1004 <Caramel1004.js@gmail.com>
2024-05-29 10:15:20 +0900

    ♻️ [refactor] Router 추가
    ==

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
-import Contaniner from './components/Header/Header.jsx';
+import MainBoardPage from './pages/MainBoard/MainBoardPage.jsx';
+import PostDetailPage from './pages/Post/PostDetailPage.jsx';
+import PostAddPage from './pages/Post/PostAddPage.jsx';
 
 function App() {
-  return (
-    <div className="App">
-      <Contaniner />
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/post/:id" element={<PostDetailPage />} exact />
+                    <Route path="/" element={<MainBoardPage />} exact />
+                    <Route path="/post/create" element={<PostAddPage />} exact />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
