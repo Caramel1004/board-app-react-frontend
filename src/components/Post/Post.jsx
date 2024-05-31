5385220fcaae87fe1a607a72f389e31631461e59
Caramel1004 <Caramel1004.js@gmail.com>
2024-05-31 22:03:59 +0900

    ✨ [feature] 게시물 세부정보 페이지로 이동하는 기능 추가
    ==

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,13 +1,20 @@
 import './Post.css';
 import docker from '../../images/docker.png'
+import { Link } from 'react-router-dom';
 
 export default function Post(props) {
     return (
         <>
-            <div className='card'>
-                <img src={docker}/>
-                <h3>{props.title}</h3>
-            </div>
+            <Link
+                className='card' 
+                to={`/post/${props.post.id}`}
+                post={props.post}
+            >
+                <img src={docker} />
+                {/* <h3>{props.post.id}</h3> */}
+                <h3>{props.post.title}</h3>
+                <h3>{props.post.description}</h3>
+            </Link>
         </>
     );
 }
