c025cf2fc4b0842add8aac4e5b414fa89b2a1317
Caramel1004 <Caramel1004.js@gmail.com>
2024-05-31 22:00:06 +0900

    ✨ [feature] 게시물 생성 기능 추가, UI 업데이트
    [feature] 게시물 생성 기능 추가, UI 업데이트

--- src/pages/Post/PostAddPage.jsx
+++ src/pages/Post/PostAddPage.jsx
@@ -1,5 +1,47 @@
+import { useEffect, useState } from 'react';
+import './PostAddPage.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 export default function PostAddPage() {
-	return(
-		<h1>게시물 추가 페이지</h1>
-	)
+    const navigate = useNavigate();
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const clickHandler = async e => {
+        e.preventDefault();
+        try {
+            console.log(title);
+            console.log(description)
+            const response = await axios.post('http://localhost:4000/boards/create', {
+                title: title,
+                description: description,
+                status: 'PUBLIC'
+            });
+            const data = response.data;
+            navigate(`/post/${data.id}`);
+        } catch (error) {
+            new Error('error');
+        }
+    }
+
+    return (
+        <>
+            <div className='container'>
+                <form className="textarea-container" onSubmit={clickHandler}>
+                    <div>
+                        <input type="text" placeholder='제목' value={title} onChange={(e) => setTitle(e.target.value)} />
+                    </div>
+                    <div className='textarea-container'>
+                        <textarea
+                            placeholder="글을 입력하세요."
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        ></textarea>
+                    </div>
+                    <button className="submit-button" type="submit">제출</button>
+                </form>
+            </div>
+        </>
+    )
 }
