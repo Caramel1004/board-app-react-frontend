1e7111c836c99e73d855bfc8aa90850f6a3595d8
Caramel1004 <Caramel1004.js@gmail.com>
2024-05-31 21:56:50 +0900

    👔 [add] 게시물 세부정보 엔드포인트 요청 로직 추가
    [add] 게시물 세부정보 엔드포인트 요청 로직 추가

--- src/pages/Post/PostDetailPage.jsx
+++ src/pages/Post/PostDetailPage.jsx
@@ -1,5 +1,36 @@
-export default function PostDetilPage() {
-	return(
-		<h1>게시물 세부</h1>
-	)
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom"
+
+export default function PostDetilPage(props) {
+    const [ post, setPost ] = useState({});
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const clickHandler = () => {
+        navigate('/');
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`http://localhost:4000/boards/${id}`);
+                
+                setPost(response.data);
+            } catch (error) {
+                new Error('데이터를 가져오지 못했습니다.');
+            }
+        }
+        fetchData();
+    },[id]);
+    
+    console.log(post)
+    
+    return (
+        <>
+            <button onClick={clickHandler}>게시물로 이동</button>
+            <h1>타이틀: {post.title}</h1>
+            <h1>내용: {post.description}</h1>
+        </>
+    )
 }
